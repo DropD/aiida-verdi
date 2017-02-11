1b79edc490d22b94b22edc8b071796a20027ee7a
DropD <r.haeuselmann@gmx.ch>
2017-02-11 21:37:27 +0100

    add test for setup, try using mock, not working.

--- aiida_verdi/tests/code/test_setup.py
+++ aiida_verdi/tests/code/test_setup.py
@@ -8,24 +8,28 @@ from click.testing import CliRunner
 import mock
 
 
-def fake_get_comp(self, comphandle):
+def fake_get_comp():
     from aiida.orm.computer import Computer
-    return Computer()
+    def get(comphandle=4):
+        return Computer()
+    return get
 
 
-def fake_comp_complete(ctx, incomplete):
+def fake_complete(self, ctx=None, incomplete=None):
     return [('Computer', None)]
 
 
-@mock.patch('aiida.orm.computer.Computer.get', new_callable=fake_get_comp)
-@mock.patch('aiida_verdi.param_types.computer.ComputerParam.complete', new_callable=fake_comp_complete, ctx=None, incomplete=None)
+@mock.patch('aiida.orm.implementation.sqlalchemy.computer.Computer', autospec=True)
+@mock.patch('aiida_verdi.param_types.computer.ComputerParam')
 @mock.patch('aiida_verdi.verdic_utils.create_code')
-def test_installed_interactive_dry(comp, crco):
+def test_installed_interactive_dry(m1, ComputerParam, m3):
+    ComputerParam.complete = fake_complete.__get__(ComputerParam)
+    m1.get = fake_get_comp()
     from aiida_verdi.commands.code import code
     runner = CliRunner()
     result = runner.invoke(
         code,
         ['setup', '--dry-run'],
-        input='Label\nDescription\ntrue\nsimpleplugins.templatereplacer\nComputer\n/Path\n\quit\n\quit\n')
+        input='Label\nDescription\ntrue\nsimpleplugins.templatereplacer\n?\nComputer\n/Path\n\quit\n\quit\n')
     assert not result.exception
     assert result.output == ''
